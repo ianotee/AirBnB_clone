e67ce3e3c2ed0a37b57defb76ddfca78b664c255
Planter44 <planterplanter777@gmail.com>
2023-11-12 05:59:51 -0800

    amenity

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is the Class for Amenity.
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """In this amenity class we need a string name.
+    """
+    name = ""
